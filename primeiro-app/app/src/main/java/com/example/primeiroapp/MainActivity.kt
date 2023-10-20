e595529a01e384514e030172760db3793d308e21
marcosrossoni <marcosrossoni9@gmail.com>
2023-10-20 16:49:05 -0300

    atribuindo ação e concatenando valores

--- primeiro-app/app/src/main/java/com/example/primeiroapp/MainActivity.kt
+++ primeiro-app/app/src/main/java/com/example/primeiroapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.primeiroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.primeiroapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonOla.setOnClickListener {
+            val nome = binding.editNome.text.toString()
+            binding.textResultado.setText("Ola $nome")
+        }
     }
 }
