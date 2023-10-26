647bed46cbae8c6865684248eae001b388f7f2ea
marcosrossoni <marcosrossoni9@gmail.com>
2023-10-25 23:08:53 -0300

    alteracao no layote do conversor

--- Coversor/app/src/main/java/com/example/coversor/MainActivity.kt
+++ Coversor/app/src/main/java/com/example/coversor/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.coversor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.coversor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +13,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonConverter.setOnClickListener {
-            val euro = binding.vlEuro.text.toString().toDouble()
-            var dolar = String.format("%.2f", euro * 1.6)
+        binding.buttonDolar.setOnClickListener {
+            escrever(0.8)
+        }
 
-            binding.vlConverter.text = "$dolar$"
+        binding.buttonPeso.setOnClickListener {
+            escrever(10.2)
+        }
+
+        binding.buttonReal.setOnClickListener {
+            escrever(5.0)
         }
     }
+
+    private fun escrever(taxa: Double) {
+        val euro = binding.editEuros.text.toString().trim()
+
+        if (!euro.isEmpty()){
+            val resultado = euro.toDouble() * taxa
+            Toast.makeText(applicationContext, "$resultado$", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
